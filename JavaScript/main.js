c24edb57c9c083fd1c41ca29b9b57dec1c48b6e3
arvinr16 <arviansyahyk@gmail.com>
2025-03-27 11:27:34 +0700

    Commit Materi Variable JavaScript

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,18 @@
-console.log("Hello, World! (external)")
+// console.log("Hello, World! (external)")
+
+// var iniVariable = "Ini adalah variable dari JavaScript";
+// console.log(iniVariable);
+// var iniVariable = "Ini variable below";
+// console.log(iniVariable);
+// var iniVariable = "Ini variable again";
+// console.log(iniVariable);
+
+// let iniVariable = "Ini adalah variable";
+// console.log(iniVariable);
+// iniVariable = "Ini adalah variable again";
+// console.log(iniVariable);
+
+const constVariable = "Ini adalah const";
+console.log(constVariable);
+// const constVariable = "Hello from const";
+// constVariable = "Hello from const again";

6a690ac4fedcda6cde5041e7b5de978338c29a58
arvinr16 <arviansyahyk@gmail.com>
2025-04-15 21:48:01 +0700

    Materi To-do List (hampir selesai)

---
+++ To-Do-List/main.js
@@ -0,0 +1,21 @@
+const list = document.getElementById("list");
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    const inputValue = e.target.children[0].value;
+    console.log(inputValue);
+
+    
+}
+
+function createListItem(inputValue) {
+    const item = document.createElement("div")
+    item.innerHTML = `
+        <div>
+            <input type="checkbox" />
+            <span>${inputValue}</span>
+        </div>
+        <button>X</button>
+    `
+    return item
+}

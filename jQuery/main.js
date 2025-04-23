526b5c0b96988519de9cfbf16d443fa145ce702c
arvinr16 <arviansyahyk@gmail.com>
2025-04-23 08:49:15 +0700

    Materi DOM JS jQuery(belum final)

--- jQuery/main.js
+++ jQuery/main.js
@@ -1,2 +1,8 @@
-console.log("Materi DOM JS");
-console.log("Backdate 22 April");
+console.log("Materi DOM JS jQuery");
+const el = $("p");
+const el1 = document.getElementsByTagName("p");
+const el2 = document.querySelector("p");
+
+console.log(el);
+console.log(el1);
+console.log(el2);

8ef8dff189960ae7ef8cea55de985c687e015eeb
arvinr16 <arviansyahyk@gmail.com>
2025-04-24 11:36:49 +0700

    Penambahan Komentar pada file 'jQuery.js'(belum final0)

--- jQuery/main.js
+++ jQuery/main.js
@@ -1,9 +1,14 @@
 console.log("Materi DOM JS jQuery");
-// const el = $("p");
-// const el1 = document.getElementsByTagName("p");
-// const el2 = document.querySelectorAll("p");
 
-const el = $("#hello");
+// const el = $("p"); = Digunakan 'jQuery' untuk memilih semua elemen '<p>' di dalam dokumen. 
+// const el1 = document.getElementsByTagName("p"); = Cara native (vanilla JavaScript) untuk memilih semua
+                                                //   elemen '<p>' dalam dokumen.
+// const el2 = document.querySelectorAll("p"); = Menggunakan 'querySelectorAll' dari JavaScript untuk memilih
+                                            //   semua elemen '<p>'.
+
+const el = $("#hello");// = Baris ini menggunakan jQuery untuk memilih elemen dengan 'id="hello"'.
+                       //   Hasilnya adalah objek 'jQuery' yang merepresentasikan elemen tersebut,
+                       //   dan bisa digunakan untuk manipulasi DOM seperti mengubah teks, atribut, atau menambahkan event.
 
 const el1 = $(".world")
 

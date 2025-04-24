ae19380b4a3fecc52db3498915fc51e2c7723cb9
arvinr16 <arviansyahyk@gmail.com>
2025-04-24 11:24:33 +0700

    Materi DOM JS jQuery(belum final0)

--- jQuery/main.js
+++ jQuery/main.js
@@ -1,8 +1,11 @@
 console.log("Materi DOM JS jQuery");
-const el = $("p");
-const el1 = document.getElementsByTagName("p");
-const el2 = document.querySelector("p");
+// const el = $("p");
+// const el1 = document.getElementsByTagName("p");
+// const el2 = document.querySelectorAll("p");
+
+const el = $("#hello");
+
+const el1 = $(".world")
 
 console.log(el);
 console.log(el1);
-console.log(el2);
